520a3bd1aaf9459155b40b2871945b41630cd00d
Bowenlinnn <46585181+Bowenlinnn@users.noreply.github.com>
2020-11-17 13:08:15 -0800

    update

--- App.js
+++ App.js
@@ -1,107 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import React, {Component} from 'react';
-import { render } from 'react-dom';
-import { 
-  ActivityIndicator, 
-  AppRegistry,
-  StyleSheet, 
-  Text, 
-  View,
-  Dimensions  } from 'react-native';
-import { getLodge } from './Client/API/index.js'
+import React from 'react';
+import { View, StatusBar } from 'react-native';
+import { Button } from '@ant-design/react-native';
 
+//page
+import Home from './pages/MainScreen'
 
-const window = Dimensions.get("window");
-const screen = Dimensions.get("screen");
-
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      data: null,
-    };
-  }
-
-  componentDidMount() {
-    //When app.js loads it will run an async func
-    this.getData();
-    //Listens for screen change
-  }
-
-  async getData() {
-    //Calls api and will finish when data is loaded
-    const { data } = await getLodge();
-    this.setState({ data });
-    console.log(data);
-  }
-  render() {
-    const { data } = this.state;
-
-    return (
-      <View style={styles.container}>
-        { //If data then display api otherwise loading indicator
-        data ? ( //if data
-          <React.Fragment>
-            {/*this is how you get 1 item
-            <Text style={styles.bold}>{data.events[0].name}</Text>
-            */}
-            
-            {/*this is how you get multiple items*/}
-            <View style={styles.eventbox}>
-              {data.events.map((lodge, i) => (
-                  <View style={styles.event}  key = {i}>
-                    <Text style = {styles.title}>{lodge.name}</Text>
-                    <Text style = {styles.location}>{lodge.location}</Text>
-                    <Text style = {styles.description}>{lodge.description}</Text>
-                  </View>
-              ))}
-            </View>
-          </React.Fragment>
-          
-          
-        ) 
-        : //else 
-        (<ActivityIndicator/>)
-        }
-      </View>
-    );
-  }
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  eventbox: {
-    flexDirection: "column",
-    flex:1,
-    width: '100%'
-  },
-  event: {
-    flexDirection: "column",
-    height: 100,
-    padding: 20,
-    borderWidth: 0,
-    borderBottomWidth: 1,
-    borderColor: 'gray',
-    borderStyle: 'solid'
-  },
-  title: {
-    fontWeight: 'bold',
-    fontSize: 22
-  },
-  description: {
-    fontSize: 16
-  },
-  location: {
-    fontSize: 16,
-    fontStyle: "italic",
-    color: 'gray'
-  }
-});
+function configureScene(route) {
+  return route.scene || Navigator.SceneConfigs.FloatFromBottom
+}
+export default function App() {
+  return (
+    <View style={{ flex: 1, }}>
+      <StatusBar
+        barStyle='light-content'
+        backgroundColor='transparent'
+        style={{ height: 20 }}
+      />
+      <Home></Home>
+    </View>
+  );
+}
 
 
---
+++ pages/MainScreen.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import { TabBar,Icon } from '@ant-design/react-native';
+
+import * as Font from 'expo-font';
+
+import Home1Page from './MySchedules' //My Events page页面
+import Home2Page from './Schedules' //Explore page页面
+import Home3Page from './Search' //Search page页面
+
+export default class MainScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedTab: 'Tab1', //The default open默认开启tab1
+    };
+  }
+
+  async componentDidMount() {
+    await Font.loadAsync(
+        'antoutline',
+        // eslint-disable-next-line
+        require('@ant-design/icons-react-native/fonts/antoutline.ttf')
+    );
+
+    await Font.loadAsync(
+        'antfill',
+        // eslint-disable-next-line
+        require('@ant-design/icons-react-native/fonts/antfill.ttf')
+    );
+    // eslint-disable-next-line
+    this.setState({ isReady: true });
+}
+
+
+  onChangeTab(tabName) {
+    this.setState({
+      selectedTab: tabName,
+    });
+  }
+  render() {
+    return (
+      <TabBar
+        unselectedTintColor="#949494"
+        tintColor="#FD7420"
+        barTintColor="#f5f5f5"
+      >
+        <TabBar.Item
+          title="My Events"
+          icon={<Icon name={"menu"} color={this.state.selectedTab=='Tab1'?"#FD7420":"#949494"} />}
+          selected={this.state.selectedTab === 'Tab1'}
+          onPress={() => this.onChangeTab('Tab1')}
+        >
+          <Home1Page></Home1Page>
+        </TabBar.Item>
+        <TabBar.Item
+          icon={<Icon name={"plus"}  color={this.state.selectedTab=='Tab2'?"#FD7420":"#949494"}  />}
+          title="Explore"
+          selected={this.state.selectedTab === 'Tab2'}
+          onPress={() => this.onChangeTab('Tab2')}
+        >
+          <Home2Page></Home2Page>
+        </TabBar.Item>
+        <TabBar.Item
+          icon={<Icon name={"search"}  color={this.state.selectedTab=='Tab3'?"#FD7420":"#949494"} />}
+          title="Search"
+          selected={this.state.selectedTab === 'Tab3'}
+          onPress={() => this.onChangeTab('Tab3')}
+        >
+          <Home3Page></Home3Page>
+        </TabBar.Item>
+      </TabBar>
+    );
+  }
+}
