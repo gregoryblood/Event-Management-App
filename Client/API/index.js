0527f79fbe479f324471d98a8439c360df382405
Gregory Blood <gregoryblood1998@gmail.com>
2021-01-29 13:34:19 -0800

    Updated api calls

--- Client/API/index.js
+++ Client/API/index.js
@@ -1,17 +1,23 @@
 import axios from 'axios'; //API Getter
 
-
-
-
 //Gets all events
 export const getEvent = () => 
     axios.get('https://osu-event-server.herokuapp.com/');
 //Gets one event
 export const getAEvent = (id) => 
-    axios.get('https://osu-event-server.herokuapp.com/'+id);
+    axios.get('https://osu-event-server.herokuapp.com/get/'+id);
 //Gets number of events
 export const getCount = () => 
     axios.get('https://osu-event-server.herokuapp.com/count');
+//Adds Attendee
+export const addAttendee = (id) => 
+    axios.get('https://osu-event-server.herokuapp.com/addattendee/'+id);
+//Remove Attendee
+export const removeAttendee = (id) => 
+    axios.get('https://osu-event-server.herokuapp.com/removeattendee/'+id);
+//Updates the max slots of an event
+export const removeAttendee = (id, num) => 
+    axios.get('https://osu-event-server.herokuapp.com/updatemaxslots/'+id+'/'+num);
 
 //Adds tracks to a given playlist
 export const addEventToList = (name, description, location) => {
