430e341dbab399e421fb7cdce6e65ba2508dd240
Carson <lengwinc@gmail.com>
2021-04-08 17:53:31 -0700

    Can sign in to app

--- App.js
+++ App.js
@@ -9,43 +9,64 @@ import Explore from './pages/Explore';
 import Search from './pages/Search';
 import Calendar from './pages/Calendar';
 
-const Tab = createBottomTabNavigator();
+import withFirebaseAuth from 'react-with-firebase-auth';
+import { firebase } from '@firebase/app'
+import 'firebase/auth';
+import firebaseConfig from './firebaseConfig';
 
-export default function App() {
-  return (
-    <NavigationContainer style={{ flex: 1}}>
-      <Tab.Navigator
-              screenOptions={({ route }) => ({
-                tabBarIcon: ({ focused, color, size }) => {
-                  let iconName;
-                  if (route.name === 'Explore') {
-                    iconName = 'ios-add-circle-outline';
-                  } else if (route.name === 'MyEvents') {
-                    iconName = 'ios-list';
-                  }
-                  else if (route.name === 'Search') {
-                    iconName = 'ios-search';
-                  }
-                  else if (route.name === 'Calendar') {
-                    iconName = 'ios-calendar';
-                  }
-                  // You can return any component that you like here!
-                  return <Ionicons name={iconName} size={35} color={color} />;
-                },
-              })}
-              tabBarOptions={{
-                activeTintColor: 'orange',
-                inactiveTintColor: 'gray',
-              }}>
-        
-        <Tab.Screen name="MyEvents" component={MyEvents} options={{title: ' '}}/>
-        <Tab.Screen name="Calendar" component={Calendar} options={{title: ' '}}/>
-        <Tab.Screen name="Explore" component={Explore} options={{title: ' '}}/>
-        <Tab.Screen name="Search" component={Search} options={{title: ' '}}/>
-      </Tab.Navigator>
-    </NavigationContainer>
+
+const Tab = createBottomTabNavigator();
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const firebaseAppAuth = firebaseApp.auth();
+const providers = {
+  googleProvider: new firebase.auth.GoogleAuthProvider(),
+};
+
+
+function App({ user, signOut, signInWithGoogle }) {
+  console.log(user);
+  return ( 
+    user ? 
+      <NavigationContainer style={{ flex: 1}}>
+        <Tab.Navigator
+                screenOptions={({ route }) => ({
+                  tabBarIcon: ({ focused, color, size }) => {
+                    let iconName;
+                    if (route.name === 'Explore') {
+                      iconName = 'ios-add-circle-outline';
+                    } else if (route.name === 'MyEvents') {
+                      iconName = 'ios-list';
+                    }
+                    else if (route.name === 'Search') {
+                      iconName = 'ios-search';
+                    }
+                    else if (route.name === 'Calendar') {
+                      iconName = 'ios-calendar';
+                    }
+                    // You can return any component that you like here!
+                    return <Ionicons name={iconName} size={35} color={color} />;
+                  },
+                })}
+                tabBarOptions={{
+                  activeTintColor: 'orange',
+                  inactiveTintColor: 'gray',
+                }}>
+          
+          <Tab.Screen name="MyEvents" component={MyEvents} options={{title: ' '}}/>
+          <Tab.Screen name="Calendar" component={Calendar} options={{title: ' '}}/>
+          <Tab.Screen name="Explore" component={Explore} options={{title: ' '}}/>
+          <Tab.Screen name="Search" component={Search} options={{title: ' '}}/>
+        </Tab.Navigator>
+      </NavigationContainer>
+      : <button onClick={signInWithGoogle}>Sign in with Google</button>
   );
 }
+
+export default withFirebaseAuth({
+  providers,
+  firebaseAppAuth,
+})(App);
 /*
 <StatusBar
         barStyle='light-content'
---
+++ firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD5kFbfUQWfRpnsUKiN1NvrDIdYx2WY8Ac",
+    authDomain: "osu-event-planner-9109a.firebaseapp.com",
+    projectId: "osu-event-planner-9109a",
+    storageBucket: "osu-event-planner-9109a.appspot.com",
+    messagingSenderId: "59510543816",
+    appId: "1:59510543816:web:894466f8a24ae3d2cd5cfd",
+    measurementId: "G-HQM7VZ1NDY"
+  };
+
+  export default firebaseConfig;
